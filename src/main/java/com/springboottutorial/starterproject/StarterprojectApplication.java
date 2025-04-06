230112508aaa32612a617bb526f2fdf7770bf8d2
philipenko <philip.boro@gmail.com>
2025-04-06 15:40:57 +0200

    initial commit

---
+++ src/main/java/com/springboottutorial/starterproject/StarterprojectApplication.java
@@ -0,0 +1,13 @@
+package com.springboottutorial.starterproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarterprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarterprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboottutorial/starterproject/StarterprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboottutorial.starterproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarterprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
